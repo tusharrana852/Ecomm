ffc674d3274e58d7a5a1e9dc6eb525031ce958de
Tushar Rana <tusharrana852@gmail.com>
2021-04-24 00:46:01 +0530

    add view engine ejs

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,7 @@
 
 
 module.exports.home=function(req,res){
-    return res.send("express is setup");
+   // return res.send("express is setup");
+    return res.render('home',{title:"Home"});
 }
 
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+const path= require('path');
 
-
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
 
 app.use('/',require('./routes'));
+
 
 
 const port = process.env.PORT||3000;
