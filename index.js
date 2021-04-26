5f0af0b5b6f7e8f8978c7ff19b6274da46d93074
Tushar Rana <tusharrana852@gmail.com>
2021-04-26 21:52:05 +0530

    adding static files

--- index.js
+++ index.js
@@ -5,10 +5,15 @@ const expressLayouts =require('express-ejs-layouts');
 
 app.use(expressLayouts);
 
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
+
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 
 app.use('/',require('./routes'));
+app.use(express.static('./assets'));
 
 
 
